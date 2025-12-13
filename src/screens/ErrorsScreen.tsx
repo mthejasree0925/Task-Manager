314af23f7bdae6e77c7b4bd21430767f74d0ef02
Thejasree Manyam <Thejasree.Manyam@evryindia.in>
2025-12-13 11:53:28 +0530

    navigation setup with basic screens

---
+++ src/screens/ErrorsScreen.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export const ErrorsScreen = () => {
+  return (
+    <View>
+      <Text>ErrorsScreen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
